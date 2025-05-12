9a4a1d012d2bd5a4dedeac51f3735147bb4b803e
Fabio B. Oliveira <155403560+fabiobaroliveira@users.noreply.github.com>
2025-05-11 21:25:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.set_page_config(
     page_title="Python AG",
     page_icon="🐍",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="auto"
 )
 
 # Título e introdução
