9ff709b786ea614607ae394868870ef36c006228
Fabio B. Oliveira <155403560+fabiobaroliveira@users.noreply.github.com>
2025-05-28 16:12:46 -0300

    Update 3_Python_Rent_a_Car.py

--- pages/3_Python_Rent_a_Car.py
+++ pages/3_Python_Rent_a_Car.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
-from datetime import date
-
+from datetime import date , timedelta 
 st.set_page_config(page_title="Python Rent a Car 🐍", layout="wide")
 
 st.title("Python Rent a Car ")
@@ -25,8 +24,8 @@ clientes_df, veiculos_df, locacoes_df,equipes_df, lojas_df = load_data()
 # Merge dos dataframes
 df = locacoes_df.merge(clientes_df, on="id_cliente") \
                 .merge(veiculos_df, on="id_veiculo") \
-                .merge(equipes_df, on="id_atendente")\
-                .merge(lojas_df, on="id_loja")               
+                .merge(lojas_df, on="id_loja")\
+                .merge(equipes_df, on="id_atendente")
                 
 
 # Alterar tipos numéricos
@@ -38,25 +37,63 @@ df["data_locacao"] = pd.to_datetime(df["data_locacao"], dayfirst=True)
 df["data_devolucao"] = pd.to_datetime(df["data_devolucao"], dayfirst=True)
 
 
-# Filtro por data
-data_hoje = date.today()
-df_hoje = df[df["data_locacao"].dt.date == data_hoje]
+# Opções de filtro
+filtro_data = st.selectbox(
+    "Filtrar por período:",
+    ("Hoje", "Últimos 7 dias", "Último mês", "Período completo")
+)
+
+# Define o intervalo de datas
+data_fim = df["data_locacao"].max().date()
+data_inicio = df["data_locacao"].min().date()
+
+if filtro_data == "Hoje":
+        data_inicio_filtro = date.today()
+        data_fim_filtro = date.today()
+elif filtro_data == "Últimos 7 dias":
+        data_inicio_filtro = date.today() - timedelta(days=6)
+        data_fim_filtro = date.today()
+elif filtro_data == "Último mês":
+        data_inicio_filtro = date.today() - timedelta(days=30)
+        data_fim_filtro = date.today()
+else:  # Período completo
+        data_inicio_filtro = data_inicio
+        data_fim_filtro = data_fim
+
+
+# Aplica o filtro
+df_filtrado = df[
+        (df["data_locacao"].dt.date >= data_inicio_filtro) &
+        (df["data_locacao"].dt.date <= data_fim_filtro)
+]
 
 # KPIs
-faturamento = df_hoje["valor_total"].sum()
-num_locacoes = df_hoje["id_locacao"].nunique()
+faturamento = df_filtrado["valor_total"].sum()
+num_locacoes = df_filtrado["id_locacao"].nunique()
 ticket_medio = faturamento / num_locacoes if num_locacoes > 0 else 0
+
+
+
+
+def format_currency(value):
+    if value >= 1_000_000:
+        return f"R$ {value / 1_000_000:,.1f} mi"
+    elif value >= 1_000:
+        return f"R$ {value / 1_000:,.1f} mil"
+    else:
+        return f"R$ {value:,.2f}"
 
 # Layout do dashboard
 col1, col2, col3, col4 = st.columns(4)
-col1.metric("Faturamento do dia", f"R$ {faturamento:,.2f}")
-col2.metric("Nº de locações", num_locacoes)
-col3.metric("Ticket médio", f"R$ {ticket_medio:,.2f}")
+col1.metric("Faturamento do período", format_currency(faturamento))
+#col2.metric
+col3.metric("Nº de locações", num_locacoes)
+col4.metric("Ticket médio", f"R$ {ticket_medio:,.2f}")
 
 
 
 # Agrupamento por loja para gerar indicadores
-df_lojas = df_hoje.groupby("nome_loja").agg(
+df_lojas = df_filtrado.groupby("nome_loja").agg(
     Faturamento=("valor_total", "sum"),
     Locações=("id_locacao", "count")
 ).sort_values("Faturamento", ascending=False).reset_index()
